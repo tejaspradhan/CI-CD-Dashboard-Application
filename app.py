b330d8245ae7dc8c59828682c61ef3037efcdd29
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-07-01 11:16:38 +0530

    completed backend

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
+import os
 app = Flask(__name__)
 
 
+def parse_file(filename):
+    f = open('./pipeline/info/'+filename, 'r')
+    attributes = filename.split('_')
+    attributes[1] = attributes[1].replace(".txt", "")
+    content = f.read().split("\n")
+    content = content[1:]
+    content[0] = content[0][1:]
+    content = content[:-1]
+    attributes += content
+    return attributes
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def dashboard():
+    info = dict()
+    filenames = [f for f in os.listdir(
+        './pipeline/info') if os.path.isfile(os.path.join('./pipeline/info', f))]
+    for name in filenames:
+        info[name[:name.find('_')]] = parse_file(name)
+    return render_template("index.html", version_info=info)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
