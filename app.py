5936f22b2f53ce5ff466c5d62082b090a807c14f
Yashbhangdia <bhangdiayash3@gmail.com>
2021-07-15 11:00:27 +0530

    Added error page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import helper
 from flask import Flask, request, render_template, url_for
+import urllib
 import os
+import json
+from json2html import *
+
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 PIPELINES = []
+ALL_VM_STATUS = []
 
 def pipelines():
     global PIPELINES
@@ -19,37 +24,63 @@ def pipelines():
                 PIPELINES.remove(x)
         else:
             PIPELINES.remove(x)
-        
+
+def all_vm_status():
+    global ALL_VM_STATUS
+    f = open('../Health_Check/logfile.txt', 'r')
+    content = f.read().split("\n")
+    for x in content:
+        if(len(x)<2):
+            continue
+        ALL_VM_STATUS.append(x.split(" : "))
+
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("error.html", status=404)
 
 @app.route('/')
 def dashboard():
-
-    return render_template("index.html", text="Please Select Project To Display Status", pipelines=PIPELINES)
+    try:
+        return render_template("index.html", text="Please Select Project To Display Status", pipelines=PIPELINES, all_status=ALL_VM_STATUS)
+    except:
+        return render_template("error.html", status=500)
 
 @app.route('/report', methods=['POST'])
 def fetch():
-    appname = request.form['app']
-    successful = 0
-    info = dict()
-    filenames = [f for f in os.listdir(
-        '../'+ appname +'/info') if os.path.isfile(os.path.join('../'+ appname +'/info', f))]
-    for name in filenames:
-        info[name[:name.find('-')]] = helper.parse_file(name, appname)
-        if(info[name[:name.find('-')]][-1] == 1):
-            successful += 1
-    latest = info[filenames[0][:filenames[0].find('-')]][2]
-    return render_template("index.html", version_info=info, percent=str((successful/len(info))*100)+"%", latest_build=latest, successful=successful, pipelines=PIPELINES, appname=appname)
+    try:
+        appname = request.form['app']
+        successful = 0
+        info = dict()
+        filenames = [f for f in os.listdir(
+            '../'+ appname +'/info') if os.path.isfile(os.path.join('../'+ appname +'/info', f))]
+        for name in filenames:
+            info[name[:name.find('-')]] = helper.parse_file(name, appname)
+            if(info[name[:name.find('-')]][-1] == 1):
+                successful += 1
+        latest = info[filenames[0][:filenames[0].find('-')]][2]
+        return render_template("index.html", version_info=info, percent=str((successful/len(info))*100)+"%", latest_build=latest, successful=successful, pipelines=PIPELINES, appname=appname)
+    except:
+        return render_template("error.html", status=500)
 
-@app.route('/temp',methods=['GET','POST'])
+@app.route('/health',methods=['GET','POST'])
 def healthcheck():
-    #return str(requests.get("http://192.168.1.8:8090/actuator/health"))
-    response = urllib.request.urlopen("http://192.168.1.8:8090/actuator/health")
-    data = response.read()
-    data= json.loads(data)
-    print(data['status'])
+    try:
+        ip = request.form['ip']
+        response = urllib.request.urlopen("http://"+ip+":8090/actuator/health")
+        data = response.read()
+        data = json.loads(data)
+        code = json2html.convert(json = data, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+        # print(code)
+        return render_template("health.html", data=data, code=code, ip=ip)
+    except:
+        return render_template("error.html", status=500)
 
-    return data
+@app.route('/error')
+def error():
+    return render_template("error.html")
 
 if __name__ == '__main__':
     pipelines()
+    all_vm_status()
     app.run(debug=True)
